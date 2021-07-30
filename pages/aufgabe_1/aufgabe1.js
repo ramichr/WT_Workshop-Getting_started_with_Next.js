7ff82a52059fb9eeda92470be7e35e55c0528cd6
Rami Cheikh Rouhou <ramicheikhrouhou670@gmail.com>
2021-07-30 21:40:37 +0200

    Aufgaben

---
+++ pages/aufgabe_1/aufgabe1.js
@@ -0,0 +1,37 @@
+import styles from '../../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Aufgabe1() {
+    return (
+        <>
+        <main className={styles.main}>
+            <h1 className={styles.title}>Aufgabe 1</h1>
+            <br></br>
+            <br></br>
+
+            <p>Hier kommt Aufgabe 1 </p>
+
+            <br></br>
+            <br></br>
+            
+            <div className={styles.btn}>
+                <button className={styles.btn_solution}>
+                    <h3>
+                        <Link href="/aufgabe_1/solution1">Lösung zur Aufgabe 1</Link>
+                    </h3>
+                </button>
+
+                <br></br>
+                <br></br>
+        
+                <button className={styles.btn_home}>
+                    <h4>
+                        <Link href="/">Zurück zur Startseite</Link>
+                    </h4>
+                </button>
+
+            </div>
+        </main>
+        </>
+    )
+}
---
+++ pages/aufgabe_3/aufgabe3.js
@@ -0,0 +1,37 @@
+import styles from '../../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Aufgabe3() {
+    return (
+        <>
+        <main className={styles.main}>
+            <h1 className={styles.title}>Aufgabe 3</h1>
+            <br></br>
+            <br></br>
+
+            <p>Hier kommt Aufgabe 3 </p>
+
+            <br></br>
+            <br></br>
+            
+            <div className={styles.btn}>
+                <button className={styles.btn_solution}>
+                    <h3>
+                        <Link href="/aufgabe_3/solution3">Lösung zur Aufgabe 3</Link>
+                    </h3>
+                </button>
+
+                <br></br>
+                <br></br>
+        
+                <button className={styles.btn_home}>
+                    <h4>
+                        <Link href="/">Zurück zur Startseite</Link>
+                    </h4>
+                </button>
+
+            </div>
+        </main>
+        </>
+    )
+}
